6654cfa3c3104f2e72770b189d0eb4d8dd52eb09
Bochkarev Ivan <ivanx86@gmail.com>
2018-09-17 04:35:56 +0600

    Add slider js

--- app/js/common.js
+++ app/js/common.js
@@ -1,47 +1,115 @@
-$(function() {
-
-    var controls = document.querySelectorAll('.controls');
-    for(var i=0; i<controls.length; i++){
-        controls[i].style.display = 'inline-block';
+// shortcut для document.querySelector (типа замена джиквери и всё такое)))
+const qS = function (el, box) {
+    if (box) {
+        if (box.querySelector(el)) return box.querySelector(el);
+    } else {
+        if (document.querySelector(el)) return document.querySelector(el);
     }
-
-    var slides = document.querySelectorAll('#slides .slide');
-    var currentSlide = 0;
-    var slideInterval = setInterval(nextSlide,2000);
-
-    function nextSlide(){
-        goToSlide(currentSlide+1);
+};
+const qSA = function (el, box) {
+    if (box) {
+        if (box.querySelectorAll(el)) return box.querySelectorAll(el);
+    } else {
+        if (document.querySelectorAll(el)) return document.querySelectorAll(el);
     }
+};
 
-    function previousSlide(){
-        goToSlide(currentSlide-1);
-    }
+if (qS(".mySlider")) document.addEventListener("DOMContentLoaded", lightSlider(3000));
 
-    function goToSlide(n){
-        slides[currentSlide].className = 'slide';
-        currentSlide = (n+slides.length)%slides.length;
-        slides[currentSlide].className = 'slide active';
-    }
+function lightSlider(interVar) {
+    // задаём дефолтные значения для счётчиков, отступов и т.п.
+    var show = 1,
+        translate = 0;
 
+    // находим в ДОМ нужные элементы
+    var dots = qS(".dots"), //кнопки контроля
+        sliderDiv = qS(".mySlider"), //родительский блок слайдера
+        slideInner = qS(".slideInner"),
+        myNodeList = qSA(".slide");
 
-    var playing = true;
-    var pauseButton = document.getElementById('pause');
+    // пишем параметры в переменные
+    var slWidth = myNodeList[0].clientWidth,
+        listLength = myNodeList.length;
 
-    function pauseSlideshow(){
-        playing = false;
-        playing = setInterval(nextSlide,2000);
-    }
+    // автостарт функции слайдера с интервалом
+    interval = setInterval(mySlider, interVar);
 
-    var next = document.getElementById('next');
-    var previous = document.getElementById('previous');
-
-    next.onclick = function(){
-        pauseSlideshow();
-        nextSlide();
+    //Аппендим элемменты навигации прямо из скрипта без вёрстки
+    for (var i = 0; i < listLength; ++i) {
+        var newLi = document.createElement('span');
+        newLi.innerHTML = '<i class="fa fa-circle"></i>';
+        newLi.addEventListener("click", function () {
+            data = this.dataset;
+            show = data.number;
+            restartSlider();
+        });
+        newLi.setAttribute("data-number", i);
+        dots.appendChild(newLi);
     };
-    previous.onclick = function(){
-        pauseSlideshow();
-        previousSlide();
+    //находим все кнопочки контроля
+    conChilds = dots.children;
+
+    // функция слайдера
+    function mySlider() {
+        if (document.hasFocus() && sliderDiv.classList.contains("fadeEffect")) {
+            if (show >= listLength) {
+                show = 0
+            }; // если текущий слайд последний, обнуляем счётчик
+            if (show < 0) {
+                show = listLength
+            }; // если счётчик слайдов отрицательный, чиним эту фигню
+
+            if (show < listLength) { //реагируем, только если счётчик слайдов имеет адекватное число
+                for (var i = 0; i < listLength; ++i) {
+                    var element = myNodeList[i],
+                        child = conChilds[i];
+                    element.style.position = "absolute";
+                    element.style.opacity = "0";
+                    child.style.color = "#999";
+                };
+                myNodeList[show].style.position = "relative";
+                myNodeList[show].style.opacity = "1";
+                conChilds[show].style.color = "#333";
+                show++;
+            };
+        };
+
+        if (document.hasFocus() && sliderDiv.classList.contains("slideEffect")) {
+            if (show >= listLength) {
+                show = 0
+            };
+
+            translate = -1 * slWidth * show + "px";
+            slideInner.style.transform = "translateX(" + translate + ")";
+            firstClone = myNodeList[0].cloneNode(true);
+            for (var i = 0; i < listLength; ++i) {
+                var child = conChilds[i];
+                child.style.color = "#999";
+            };
+            conChilds[show].style.color = "#333";
+            show++;
+        };
     };
 
-});
+    function restartSlider() {
+        mySlider();
+        clearInterval(interval);
+        interval = setInterval(mySlider, interVar);
+    }
+
+    //прокрутка кнопками вправо и влево
+    qS(".left").addEventListener("click", function () {
+        show -= 2;
+        if (show < 0) {
+            show = listLength - 1
+        };
+        restartSlider();
+    });
+    qS(".right").addEventListener("click", function () {
+        if (show >= listLength) {
+            show = 0
+        };
+        restartSlider();
+    });
+
+};
